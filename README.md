a6d25fade413bb8d06068bfe7f2b5269dca69f72
oliUmbi <oliver.umbricht@hispeed.ch>
2025-05-14 00:02:26 +0200

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # NeoREM
 Radiation Exposure Monitoring inspired by OpenREM
+
+## Technology
+- Java
+  - Spring Boot
+- React
+- Sqlite
+  - Flyway
+- Docker
+
+## Deployment
+
+## Development
+
+### Features
+- [ ] Dicom import
+- [ ] HL7 import
+- [ ] user interface
+- [ ] csv export
+- [ ] xlsx export
---
+++ src/main/java/ch/oliumbi/neorem/NeoREM.java
@@ -0,0 +1,12 @@
+package ch.oliumbi.neorem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoREM {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoREM.class, args);
+    }
+}
